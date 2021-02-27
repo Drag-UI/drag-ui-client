541bc81d940e1e8b5f304861436d67b864b9f030
NomadHash <badsoap141@gmail.com>
2021-02-28 00:22:52 +0900

    Update/build-page: rename page

---
+++ pages/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from 'react';
+import LayoutPage from '../components/layout/pages/LayoutPage';
+
+// build page
+const Build = (): ReactElement => {
+  return (
+    <>
+      <LayoutPage />
+    </>
+  );
+};
+
+export default Build;
